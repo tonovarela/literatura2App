1b185ca09004aafcf4d1b175cb3a909470ca6fbe
tonovarela <tonovarela@live.com>
2025-11-25 13:54:19 -0600

    Se actualiza la URL del entorno de desarrollo para apuntar a la nueva dirección del servicio.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ const config: SocketIoConfig = { url: 'http://192.168.2.222:8085', options: {} }
 export const environment = {
   production: false,
   sockectConfig: config,  
-  URL: 'http://servicios.litoprocess.com/literatura',    
+  URL: 'http://servicios.litoprocess.com/literatura7',    
   delayTimeAllowedInput: 200,
   puederPegarTexto: true,
   //URL_IMPRESIONES: "http://localhost:8050"  
